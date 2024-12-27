0ebb0b6a878c4c150164753da87b7db962b1de50
al1can <ac.gunduz7878@gmail.com>
2024-12-27 17:11:27 +0300

    removed build folders

--- build/externals/faker-cxx/CMakeFiles/faker-cxx.dir/src/common/WeatherHelper.cpp.o.d
+++
@@ -1,4 +0,0 @@
-externals/faker-cxx/CMakeFiles/faker-cxx.dir/src/common/WeatherHelper.cpp.o: \
- /home/alicangndz/Desktop/prolab3/externals/faker-cxx/src/common/WeatherHelper.cpp \
- /usr/include/stdc-predef.h \
- /home/alicangndz/Desktop/prolab3/externals/faker-cxx/src/common/WeatherHelper.h
--- build_old/externals/faker-cxx/CMakeFiles/faker-cxx.dir/src/common/WeatherHelper.cpp.o.d
+++
@@ -1,4 +0,0 @@
-externals/faker-cxx/CMakeFiles/faker-cxx.dir/src/common/WeatherHelper.cpp.o: \
- /home/alicangndz/Downloads/prolab3/externals/faker-cxx/src/common/WeatherHelper.cpp \
- /usr/include/stdc-predef.h \
- /home/alicangndz/Downloads/prolab3/externals/faker-cxx/src/common/WeatherHelper.h
